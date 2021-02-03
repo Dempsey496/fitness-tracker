83860a9ce6517e05ee28524870271d3d1808fd26
Dempsey FP <dempseyfinley@gmail.com>
2021-02-03 15:27:18 -0500

    created MongoDB models

---
+++ models/workouts.js
@@ -0,0 +1,53 @@
+const opts = { toJSON: { virtuals: true } };
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+    unique: true,
+  },
+  exercises: [
+
+    {
+      name: {
+        type: String,
+        unique: false,
+      },
+
+      type: {
+        type: String,
+
+      },
+      duration: {
+        type: Number,
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+
+      },
+    }
+  ],
+}, opts);
+WorkoutSchema.virtual('totalDuration').get(function () {
+  let totalTime = 0
+  for (let i = 0; i < this.exercises.length; i++) {
+    totalTime += this.exercises[i].duration;
+  }
+  return totalTime;
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const path = require("path");
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
